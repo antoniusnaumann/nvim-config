c2eead01170420a44319013ee8c29d28aac49b27
Antonius Naumann <git@antonius.dev>
2024-02-05 00:01:24 +0100

    Add ayu theme

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,47 @@
+return {
+    -- 'arzg/vim-colors-xcode',
+    {
+      "folke/tokyonight.nvim",
+      lazy = false,
+      priority = 1000,
+      opts = {
+        transparent = true,
+      },
+    },
+    {
+      'AlexvZyl/nordic.nvim',
+      lazy = false,
+      priority = 1000,
+      config = function()
+          require 'nordic' .setup {
+              bold_keywords = true,
+              italic_comments = true,
+              transparent_bg = true,
+              leap = {
+                  dim_backdrop = false,
+              },
+          }
+      end
+    },
+    {
+      'Luxed/ayu-vim',
+    },
+    {
+      'f-person/auto-dark-mode.nvim',
+      config = {
+        update_interval = 1000,
+        set_dark_mode = function()
+          vim.cmd("set termguicolors")
+          vim.api.nvim_set_option("background", "dark")
+          vim.g.ayucolor = "dark"
+          vim.cmd("colorscheme ayu")
+        end,
+        -- TODO: Look for a cool light theme to use here
+        set_light_mode = function()
+          vim.cmd("set termguicolors")
+          vim.api.nvim_set_option("background", "light")
+          vim.cmd("colorscheme ayu")
+        end,
+    },
+  }
+}
