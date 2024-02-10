9ff482abeb668c5fadd102749bb1450c271b67a0
Antonius Naumann <git@antonius.dev>
2024-02-10 14:54:15 +0100

    Enable telescope borders

--- init.lua
+++ init.lua
@@ -5,12 +5,34 @@ require("keymap")
 
 Colors = {}
 function SetBaseColors(light)
-  Colors = {
-    red = '#ff453a',
-    yellow = '#ffd60a',
-    cyan = '#5ac8f5',
-    magenta = '#bf3fbd',
-  }
+  if light then
+    -- TODO: edit light mode colors
+    Colors = {
+      bg = '#fafafa',
+      fg = '#f2f2f7',
+      red = '#ff453a',
+      orange = '#ff9f0a',
+      yellow = '#ffd60a',
+      lime = '#7dd70f',
+      mint = '#41d496',
+      cyan = '#5ac8f5',
+      purple = '#d189f5',
+      magenta = '#bf3fbd',
+    }
+  else
+    Colors = {
+      bg = '#1c1c1e',
+      fg = '#f2f2f7',
+      red = '#ff453a',
+      orange = '#ff9f0a',
+      yellow = '#ffd60a',
+      lime = '#7dd70f',
+      mint = '#41d496',
+      cyan = '#5ac8f5',
+      purple = '#d189f5',
+      magenta = '#bf3fbd',
+    }
+  end
 end
 
 SetBaseColors(false)
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,36 +1,45 @@
 local function updateColors(light)
   SetBaseColors(light)
-  local bg
-  if light then bg = '#dedede' else bg = '#1c1c1e' end
   require('base16-colorscheme').setup({
-    base00 = bg,
-    base01 = '#636366',     -- gray, line selection in file explorer
-    base02 = '#212124',     -- gray, some bars
+    base00 = Colors.bg,
+    base01 = '#636366',     -- light gray, line selection in file explorer
+    base02 = '#212124',     -- very dark gray, some bars
     base03 = '#48484a',     -- dark gray, comments
-    base04 = '#aeaeb2',     -- light gray, line numbers
-    base05 = '#f2f2f7',     -- white, unhighlighted text
+    base04 = '#aeaeb2',     -- very light gray, line numbers
+    base05 = Colors.fg,     -- unhighlighted text
     base06 = '#ffffff',     -- ???
     base07 = '#ffffff',     -- ???
     base08 = '#1a94ff',     -- blueish white, variables
-    base09 = '#d189f5',     -- purple, number literals
-    base0A = '#41d496',     -- greenish mint, class names
-    base0B = '#7dd70f',     -- lime, string literals
-    base0C = Colors.yellow, -- yellow, warnings
+    base09 = Colors.purple, -- number literals
+    base0A = Colors.mint,   -- class names
+    base0B = Colors.lime,   -- string literals
+    base0C = Colors.yellow, -- warnings
     base0D = '#ffea80',     -- yellowish white, function calls
-    base0E = '#ff9f0a',     -- orange, keywords
+    base0E = Colors.orange, -- keywords
     base0F = '#ffffff',     -- , commas
+  }, { 
+    telescope_borders = true,
+    })
+  require('ayu').setup({
+    overrides = {
+      Normal = { bg = "None" },
+    }
   })
   -- TODO: Tweak visual mode highlights
   -- TODO: Tweak telescope borders
   -- TODO: Tweak flash label colors
+  -- TODO: Rust color clashes:
+  --        - local variables, parameters, qualified paths and macro invocations
+  --        - trait names and keywords (LSP semantic highlighting only)
+  --        - lifetimes and class names
 end
 
 return {
   {
-    'Luxed/ayu-vim',
+    'Shatur/neovim-ayu',
   },
   {
-    'RRethy/nvim-base16',
+    'RRethy/nvim-base16'
   },
   {
     'f-person/auto-dark-mode.nvim',
@@ -41,14 +50,19 @@ return {
         vim.api.nvim_set_option("background", "dark")
         vim.g.ayucolor = "dark"
         vim.cmd("colorscheme ayu")
+
         updateColors(false)
       end,
-      -- TODO: Look for a cool light theme to use here
       set_light_mode = function()
         vim.cmd("set termguicolors")
-        vim.api.nvim_set_option("background", "dark")
+        -- vim.api.nvim_set_option("background", "light")
         -- vim.cmd("colorscheme ayu")
-        updateColors(true)
+        -- TODO: Set scheme to light mode here
+        vim.api.nvim_set_option("background", "dark")
+        vim.g.ayucolor = "dark"
+        vim.cmd("colorscheme ayu")
+
+        updateColors(false)
       end,
     },
   }
